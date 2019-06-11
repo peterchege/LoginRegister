ebbde9ad4bf6cd071c155d4218c0d726b713ec58
peterkinuthia <peterchege442@gmail.com>
2019-06-11 23:13:07 +0300

    validation fields

--- app/src/main/java/com/example/herihomes/RegisterActivity.java
+++ app/src/main/java/com/example/herihomes/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.herihomes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 
@@ -31,6 +32,39 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         String password = editTextPassword.getText().toString().trim();
         String name = editTextName.getText().toString().trim();
         String school = editTextSchool.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Enter a Vaalid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            editTextPassword.setError("Password should be atleast 6 character long");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(name.isEmpty()){
+            editTextName.setError("Name Required");
+            editTextName.requestFocus();
+            return;
+        }
+        if(school.isEmpty()){
+            editTextSchool.setError("School Required");
+            editTextSchool.requestFocus();
+            return;
+        }
+
+        /* Do user registration using the api call*/
     }
     @Override
     public void onClick(View v) {
