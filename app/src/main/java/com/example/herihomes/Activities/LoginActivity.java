19512653011cbc033b192c17c9eeefc78283e1b7
peterkinuthia <peterchege442@gmail.com>
2019-06-12 20:13:07 +0300

    Login validation

--- app/src/main/java/com/example/herihomes/Activities/LoginActivity.java
+++ app/src/main/java/com/example/herihomes/Activities/LoginActivity.java
@@ -3,14 +3,19 @@ package com.example.herihomes.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.herihomes.R;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +23,55 @@ public class LoginActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
+        editTextEmail = findViewById(R.id.eT_email);
+        editTextPassword = findViewById(R.id.eT_password);
+
         final ImageView iV_houseLogin =(ImageView)findViewById(R.id.iV_houseLogin);
         iV_houseLogin.setImageResource(R.drawable.house);
 
-        final Button bt_login = (Button)findViewById(R.id.bt_login);
-        bt_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                LoginActivity.this.startActivity(intent);
-            }
-        });
+       findViewById(R.id.bt_login).setOnClickListener(this);
+       findViewById(R.id.tv_register).setOnClickListener(this);
 
+
+    }
+
+    private void userLogin(){
+
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Enter a Valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            editTextPassword.setError("Password should be atleast 6 character long");
+            editTextPassword.requestFocus();
+            return;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_login:
+                userLogin();
+                break;
+
+            case R.id.tv_register:
+                break;
+        }
 
     }
 }
