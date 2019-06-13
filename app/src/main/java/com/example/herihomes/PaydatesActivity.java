11db4a440b96f7d972ef78fd6cff1d2d5acf4f61
peterkinuthia <peterchege442@gmail.com>
2019-06-13 18:32:28 +0300

    Paydates layout

---
+++ app/src/main/java/com/example/herihomes/PaydatesActivity.java
@@ -0,0 +1,13 @@
+package com.example.herihomes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaydatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paydates);
+    }
+}
